ba851b5775eb7cff408e1ece8b58d275fb9d649b
habinski <matthewschmtz@gmail.com>
2020-07-05 19:26:29 +0200

    Components & Databinding

--- src/app/shared/ingridient.model.ts
+++
@@ -1,9 +0,0 @@
-export class Ingridient {
-	// public name: string;
-	// public amount: number;
-
-	constructor(public name: string, public amount: number) {
-		// this.name = name;
-		// this.amount = amount;
-	}
-}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingridient } from '../shared/ingridient.model';
+
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingridients: Ingridient[] = [
-    new Ingridient('Apples', 5),
-    new Ingridient('Tomatos', 10)
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
   ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
